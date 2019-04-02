eaee9a2a52d7d4a6c7ace45fe40e4cbc6425f652
boa <boa.matule@gmail.com>
2019-04-02 12:15:41 +0200

    setups expired card method to withdraw. All test green.

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,13 +2,13 @@ require './lib/atm.rb'
 require 'date'
 
 describe Atm do 
-    let(:account) { instance_double('Account', pin_code: '1234') }
+    let(:account) { instance_double('Account', pin_code: '1234', exp_date: '11/19') }
     before do
         allow(account).to receive(:balance).and_return(100)
         allow(account).to receive(:balance=)
     end
 
-    it 'has 1000$ on initialize' do
+    it 'has $1000 on initialize' do
         expect(subject.funds).to eq 1000
     end
 
@@ -36,6 +36,12 @@ describe Atm do
     it 'reject withdraw if pin is wrong' do
         expected_output = { status: false, message: 'wrong pin', date: Date.today }
         expect(subject.withdraw(50, 9999, account)).to eq expected_output
+    end
+
+    it 'reject withdraw if card is expired' do
+      allow(account).to receive(:exp_date).and_return('12/15')
+      expected_output = { status: false, message: 'card expired', date: Date.today }
+      expect(subject.withdraw(6, '1234', account)).to eq expected_output
     end
 
 end
