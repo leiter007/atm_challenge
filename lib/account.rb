d3a2b48a2b2776ef4d9ba0267c39d74b85ba66a5
boa <boa.matule@gmail.com>
2019-04-02 21:39:36 +0200

    adds expire date method and test.

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,12 @@
 class Account
-    attr_accessor :pin_code, :owner, :account_status
+
+    STANDARD_VALIDITY_YRS = 5
+
+    attr_accessor :pin_code, :owner, :account_status, :exp_date
     def initialize (attrs = {})
         @pin_code = rand(1000..9999)
         @account_status = :active
+        @exp_date =  set_expire_date 
         set_owner(attrs[:owner])
     end
 
@@ -14,5 +18,9 @@ class Account
 
     def missing_owner
     raise 'An Account owner is required'
+    end
+
+    def set_expire_date
+        Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
     end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -21,4 +21,10 @@ describe Account do
     it 'is expected to have :active status on initialize' do
         expect(subject.account_status).to eq :active
     end
+
+    it 'is expected to have an expiry date on initialize' do
+        expected_date = Date.today.next_year(5).strftime("%m/%y")
+        expect(subject.exp_date).to eq expected_date
+    end
+
 end
