14fb0c903476cd017d03cd394ab4fed3fcd6df73
felix_bonnier <felix_bonnier@yahoo.com>
2019-04-03 12:16:22 +0200

    adds user story for Person to create account, setup of account attribute

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,27 @@
 require './lib/account'
 
 class Person
-  attr_accessor :name
+  attr_accessor :name, :cash, :account
   def initialize(attrs = {})
   @name = set_name(attrs[:name])
+  @cash = 0
+  @account = nil
   end 
 
-private 
+def create_account
+  @account = Account.new(owner: self)
+end
+
+private
 
 def set_name(name)
     name == nil ? missing_name : name
-  end
+end
+
+def missing_name
+  raise 'A name is required'
+end
+
+
 
 end
