d6abd9ec3ba2ed434c3f2faa5b37327fb4be985a
boa <boa.matule@gmail.com>
2019-04-03 16:38:36 +0200

    adds methods for missing atm and increase cash

--- lib/person.rb
+++ lib/person.rb
@@ -20,6 +20,7 @@ def withdraw(args = {})
   @account == nil ? missing_account : withdraw_funds(args)
 end
 
+
 private
 
 def deposit_funds(amount)
@@ -39,6 +40,16 @@ def missing_account
   raise 'No account present'
 end
 
+def missing_atm
+    raise RuntimeError, 'An ATM is required'
+end
+
+def increase_cash(response)
+    @cash += response[:amount]
+end
+
+
+
 def withdraw_funds(args)
   args[:atm] == nil ? missing_atm : atm = args[:atm]
   account = @account
@@ -47,5 +58,4 @@ def withdraw_funds(args)
   response = atm.withdraw(amount, pin, account)
   response[:status] == true ? increase_cash(response) : response
 end
-
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -50,11 +50,24 @@ describe Person do
             command = lambda { subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account, atm: atm) }
             expect(command.call).to be_truthy
         end
-    end
+
+        it 'withdraw is expected to raise error if no ATM is passed in' do
+            command = lambda { subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account) }
+            expect { command.call }.to raise_error 'An ATM is required'
+        end
+
+        it 'funds are added to cash - deducted from account balance' do
+            subject.cash = 100
+            subject.deposit(100)
+            subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account, atm: atm)
+            expect(subject.account.balance).to be 0
+            expect(subject.cash).to be 100
+        end
+    end     
 
     describe 'can not manage funds if no account has been created' do
         it 'can not deposit funds' do
-          expect { subject.deposit(100) }.to raise_error(RuntimeError, 'No account present')
+        expect { subject.deposit(100) }.to raise_error(RuntimeError, 'No account present')
         end
     end
 end
